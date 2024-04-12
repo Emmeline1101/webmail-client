598edc3fb4f40f74aee342582374d6b9f2665aa0
Emmeline1101 <emmelinexu23@gmail.com>
2024-04-12 12:32:03 -0700

    modify version

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,38 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
-  entry : "./src/code/main.tsx",
-  devServer: {
-    host: 'localhost', // 或者 0.0.0.0 允许从外部访问
-    port: 8080, // 你想要的端口号
-    // ...
+  entry: "./src/code/main.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
   },
-
-  resolve : {
-    extensions : [ ".ts", ".tsx", ".js" ]
-  },
-
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.html$/,
-        use : { loader : "html-loader" }
+        test: /\.html$/,
+        use: { loader: "html-loader" }
       },
       {
-        test : /\.css$/,
-        use : [ "style-loader", "css-loader"]
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       },
       {
-        test : /\.tsx?$/,
+        test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
       }
     ]
-
   },
-
-  plugins : [
-    new HtmlWebPackPlugin({ template : "./src/index.html", filename : "./index.html" })
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
   ],
-
-  performance : { hints : false },
-  watch : true,
-  devtool : "source-map"
-
+  performance: {
+    hints: false
+  },
+  devtool: "source-map",
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+  },
 };
